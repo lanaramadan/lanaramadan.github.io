5582b54c91fed65f6ff48da9e5d6fa99326f1143
Lana Ramadan <lanamramadan@gmail.com>
2025-06-28 02:20:08 -0700

    Work page layout

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,10 +1,77 @@
 import NavigationBar from "../components/navigation/navigationBar";
 import Footer from "../components/navigation/footer";
+import MiniProjectCard from "../components/miniProjectCard";
+
+import projectData from "../../../public/data/projects.json";
 
 export default function Work() {
+  const programmingProjects = projectData.programmingProjects;
+  const designProjects = projectData.designProjects;
+
   return (
-    <div className="flex flex-col min-h-screen py-8 px-12 gap-16 font-[family-name:var(--font-geist-sans)]">
-      <NavigationBar/>
+    <div className="min-h-screen py-8 px-12">
+        <NavigationBar/>
+
+      <div
+        className="
+          w-full 
+          flex 
+          flex-col
+          gap-12
+          justify-between 
+          items-center
+          pb-10
+          px-12
+        "
+      >
+        <section>
+          <h2 className="
+            font-medium
+            text-xl
+            sm:text-4xl
+            font-[family-name:var(--font-new-spirit-condensed)]
+          ">Programming</h2>
+          
+          <div className="flex gap-5 w-[88vw] overflow-x-auto">
+            {programmingProjects.map((project, index) => (
+              <MiniProjectCard
+                key={index}
+                project={project.name}
+                imageSrc="/placeholder.png"
+                link={project.links[0]}
+                desktop={true}
+              />
+            ))}
+          </div>
+        </section>
+
+        <hr className="border-t border-2 border-current w-[88vw]" />
+        
+        <section>
+          <h2 className="
+            font-medium
+            text-xl
+            sm:text-4xl
+            font-[family-name:var(--font-new-spirit-condensed)]
+          ">Product Design</h2>
+          
+          <div className="flex gap-5 w-[88vw] overflow-x-auto">
+            {designProjects.map((project, index) => (
+              <MiniProjectCard
+                key={index}
+                project={project.name}
+                imageSrc="/placeholder.png"
+                link={project.links[0]}
+                desktop={true}
+              />
+            ))}
+          </div>
+        </section>
+
+
+
+      </div>
+
       <Footer />
     </div>
   );

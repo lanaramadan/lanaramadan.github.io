03f955b36cb13dc393dfcd61f88124d5c8602a8a
Lana Ramadan <lanamramadan@gmail.com>
2025-06-28 00:33:23 -0700

    Adding infinite scroll for positions

--- src/app/components/positions.tsx
+++ src/app/components/positions.tsx
@@ -4,28 +4,30 @@ import PencilDownIcon from "./icons/pencilDownIcon";
 
 export default function Positions() {
   return (
-    <div className="mt-5 flex gap-3">
-      <h1 className="text-2xl font-[family-name:var(--font-geist-mono)]">
-        application designer
-      </h1>
-      <PencilUpIcon />
-      <h1 className="text-2xl font-[family-name:var(--font-geist-mono)]">
-        software engineer
-      </h1>
-      <PencilDownIcon />
-      <h1 className="text-2xl font-[family-name:var(--font-geist-mono)]">
-        creative code
-      </h1>
-      <PencilUpIcon />
-      <h1 className="text-2xl font-[family-name:var(--font-geist-mono)]">
-        curious thinker
-      </h1>
-      <PencilDownIcon />
-      <h1 className="text-2xl font-[family-name:var(--font-geist-mono)]">
-        product designer
-      </h1>
-      <PencilUpIcon />
-      
+    <div className="overflow-hidden w-full">
+      <div className="animate-scroll whitespace-nowrap mt-5 flex gap-3 text-3xl items-center font-[family-name:var(--font-geist-mono)]">
+        <h1>application designer</h1>
+        <PencilUpIcon />
+        <h1>software engineer</h1>
+        <PencilDownIcon />
+        <h1>creative coder</h1>
+        <PencilUpIcon />
+        <h1>curious thinker</h1>
+        <PencilDownIcon />
+        <h1>product designer</h1>
+        <PencilUpIcon />
+
+        <h1>application designer</h1>
+        <PencilUpIcon />
+        <h1>software engineer</h1>
+        <PencilDownIcon />
+        <h1>creative coder</h1>
+        <PencilUpIcon />
+        <h1>curious thinker</h1>
+        <PencilDownIcon />
+        <h1>product designer</h1>
+        <PencilUpIcon />
+      </div>
     </div>
   );
 }

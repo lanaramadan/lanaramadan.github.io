b187a37cc4478ddf0e13e0d6a8e38472f961ef2e
Lana Ramadan <70675417+lanaramadan@users.noreply.github.com>
2025-08-28 14:30:41 -0700

    Add email sending functionality on page load
    Implement email sending on first visit using EmailJS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
+"use client";
+import emailjs from "emailjs-com";
+
+import { useEffect } from "react";
+
 import NavigationBar from "./components/navigation/navigationBar";
 import Footer from "./components/navigation/footer";
 import FeaturedProjects from "./components/home-page/featuredProjects";
 import HomeTitle from "./components/home-page/homeTitle";
 
 export default function Home() {
+  useEffect(() => {
+    // send email if not already sent this session
+    if (!sessionStorage.getItem("visitEmailSent")) {
+      fetch("https://api.ipify.org?format=json")
+        .then((res) => res.json())
+        .then((data) => {
+          emailjs
+            .send(
+              "service_2k3k1zd",
+              "template_3zr0nvl",
+              { ip: data.ip,
+                referrer: document.referrer || "direct",
+                time: new Date().toISOString()
+               },
+              "M53J8EdHSK4oDYC3I"
+            )
+            .then(
+              () => {
+                console.log("Email sent");
+                sessionStorage.setItem("visitEmailSent", "true");
+              },
+              (error) => {
+                console.error("Email failed", error.text);
+              }
+            );
+        });
+    }
+  }, []);
+
   return (
     <main className="">
       {/* top banner */}

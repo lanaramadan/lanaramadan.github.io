1c4ad8bb70cd8a9b8e0c880c5b979b0fab102574
Lana Ramadan <lanamramadan@gmail.com>
2026-01-24 02:37:54 -0800

    Adding letter to footer

---
+++ src/app/components/closedLetter.tsx
@@ -0,0 +1,362 @@
+type ClosedLetterProps = {
+  rotate?: number; // degrees
+};
+
+export default function ClosedLetter({ rotate = 0 }: ClosedLetterProps) {
+  return (
+    <svg
+      width="680"
+      height="451"
+      viewBox="0 0 680 451"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ transform: `rotate(${rotate}deg)` }}
+    >
+      <g filter="url(#filter0_d_2036_1033)">
+        <rect
+          x="75"
+          y="75"
+          width="529.97"
+          height="300.316"
+          fill="url(#paint0_linear_2036_1033)"
+        />
+        <g clip-path="url(#clip0_2036_1033)">
+          <g filter="url(#filter1_di_2036_1033)">
+            <path
+              d="M339.985 225.158L75.0001 378.147V72.169L339.985 225.158Z"
+              fill="#DBDCD4"
+            />
+          </g>
+          <g filter="url(#filter2_di_2036_1033)">
+            <path
+              d="M339.985 225.158L604.97 378.147V72.169L339.985 225.158Z"
+              fill="#DBDCD4"
+            />
+          </g>
+        </g>
+        <g clip-path="url(#clip1_2036_1033)">
+          <g filter="url(#filter3_di_2036_1033)">
+            <path
+              d="M335.8 180.523C338.667 178.661 342.363 178.661 345.23 180.523L621.99 360.284C629.217 364.979 625.893 376.2 617.275 376.2H63.7552C55.1371 376.2 51.8128 364.979 59.0402 360.284L335.8 180.523Z"
+              fill="url(#paint1_linear_2036_1033)"
+            />
+          </g>
+        </g>
+        <g clip-path="url(#clip2_2036_1033)">
+          <g filter="url(#filter4_di_2036_1033)">
+            <path
+              d="M335.8 269.793C338.667 271.655 342.363 271.655 345.23 269.793L621.99 90.0322C629.217 85.3379 625.893 74.1167 617.275 74.1167H63.7552C55.1371 74.1167 51.8128 85.3379 59.0402 90.0322L335.8 269.793Z"
+              fill="url(#paint2_linear_2036_1033)"
+            />
+          </g>
+        </g>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_2036_1033"
+          x="0"
+          y="0"
+          width="679.97"
+          height="450.316"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation="37.5" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.45098 0 0 0 0 0.517647 0 0 0 0 0.435294 0 0 0 0.5 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2036_1033"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2036_1033"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_di_2036_1033"
+          x="73.9403"
+          y="70.579"
+          width="267.105"
+          height="309.865"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-0.52997" />
+          <feGaussianBlur stdDeviation="0.52997" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2036_1033"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2036_1033"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2.29654" />
+          <feGaussianBlur stdDeviation="1.50158" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_2036_1033"
+          />
+        </filter>
+        <filter
+          id="filter2_di_2036_1033"
+          x="333.095"
+          y="64.5727"
+          width="278.764"
+          height="319.757"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-0.706627" />
+          <feGaussianBlur stdDeviation="3.4448" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2036_1033"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2036_1033"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2.29654" />
+          <feGaussianBlur stdDeviation="1.50158" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_2036_1033"
+          />
+        </filter>
+        <filter
+          id="filter3_di_2036_1033"
+          x="54.3766"
+          y="179.127"
+          width="572.277"
+          height="198.486"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="0.706627" />
+          <feGaussianBlur stdDeviation="0.353313" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2036_1033"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2036_1033"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2.64985" />
+          <feGaussianBlur stdDeviation="0.52997" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_2036_1033"
+          />
+        </filter>
+        <filter
+          id="filter4_di_2036_1033"
+          x="54.3766"
+          y="74.1167"
+          width="572.277"
+          height="198.486"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="0.706627" />
+          <feGaussianBlur stdDeviation="0.353313" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2036_1033"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_2036_1033"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2.64985" />
+          <feGaussianBlur stdDeviation="0.52997" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect2_innerShadow_2036_1033"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_2036_1033"
+          x1="339.985"
+          y1="75"
+          x2="339.985"
+          y2="375.316"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#DBDCD4" />
+          <stop offset="1" stopColor="#B5BCB0" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_2036_1033"
+          x1="340.515"
+          y1="177.461"
+          x2="344.136"
+          y2="373.903"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#B5BCB0" />
+          <stop offset="1" stopColor="#DBDCD4" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_2036_1033"
+          x1="340.515"
+          y1="272.855"
+          x2="340.515"
+          y2="73.4101"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#B5BCB0" />
+          <stop offset="1" stopColor="#DBDCD4" />
+        </linearGradient>
+        <clipPath id="clip0_2036_1033">
+          <rect
+            width="529.97"
+            height="300.316"
+            fill="white"
+            transform="translate(75 75)"
+          />
+        </clipPath>
+        <clipPath id="clip1_2036_1033">
+          <rect
+            width="529.97"
+            height="197.855"
+            fill="white"
+            transform="translate(75 177.461)"
+          />
+        </clipPath>
+        <clipPath id="clip2_2036_1033">
+          <rect
+            width="529.97"
+            height="197.855"
+            fill="white"
+            transform="matrix(1 0 0 -1 75 272.855)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
--- src/app/components/navigation/footer.tsx
+++ src/app/components/navigation/footer.tsx
@@ -2,18 +2,20 @@ import GithubIcon from "../icons/githubIcon";
 import LinkedinIcon from "../icons/linkedinIcon";
 import MailIcon from "../icons/mailIcon";
 import BehanceIcon from "../icons/behanceIcon";
+import ClosedLetter from "../closedLetter";
 
 export default function Footer() {
   return (
-    <div className="sticky top-0 text-dark bg-background overflow-hidden flex flex-col relative w-full h-screen justify-center px-20">
+    <div className="sticky top-0 text-dark bg-background overflow-hidden flex relative w-full h-screen justify-center items-center gap-24 px-20">
       <div className="flex flex-col gap-8 text-5xl font-serif">
         <p>Let's keep in <span className="font-imperial-script text-7xl">touch</span>!</p>
         <div className="flex gap-12">
           <p className="font-ibm-plex-sans text-xl hover:underline hover:drop-shadow-[0_0_8px_currentColor]">Email ↗</p>
           <p className="font-ibm-plex-sans text-xl hover:underline hover:drop-shadow-[0_0_8px_currentColor]">Linkedin ↗</p>
         </div>
-
       </div>
+
+      <ClosedLetter rotate={-11}/>
     </div>
   );
 }
